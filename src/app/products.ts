b20efda2b33f3e5d94f07f11c333072dc9e5eaa9
tshivam <tshivam.iitkgp@gmail.com>
2021-06-01 21:15:58 +0530

    7 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,6 +3,7 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  img: string;
 }
 
 export const products = [
@@ -10,19 +11,24 @@ export const products = [
     id: 1,
     name: '10 GPU Rig',
     price: 500000,
-    description: 'A GPU mining rig consisting of 10 GTX 1660 Super'
+    description: 'A GPU mining rig consisting of 10 GTX 1660 Super',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/81nbugDQ1lL._SL1500_.jpg'
   },
   {
     id: 2,
     name: '6 GPU Rig',
     price: 320000,
-    description: 'A GPU mining rig consisting of 6 GTX 1660 Super'
+    description: 'A GPU mining rig consisting of 6 GTX 1660 Super',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/71EVunv9GaL._AC_SL1200_.jpg'
   },
   {
     id: 3,
     name: '4 GPU Rig',
     price: 250000,
-    description: ''
+    description: 'A GPU mining rig consisting of 4 GTX 1660 Super',
+    img: 'https://i.redd.it/871cd2t0b8531.jpg'
   }
 ];
 
